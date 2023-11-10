125db37ca893477e023eac954c06547ace037d13
김대영 <ssafy@ssafy>
2023-11-10 13:55:50 +0900

    Feat : Footer 추가

--- enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/App.vue
+++ enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/App.vue
@@ -1,13 +1,25 @@
 <script setup>
 import { RouterView } from "vue-router";
 import TheHeadingNavbar from "@/components/layout/TheHeadingNavbar.vue";
+import Footer from "@/components/layout/Footer.vue";
 </script>
 
 <template>
-  <div>
-    <TheHeadingNavbar></TheHeadingNavbar>
-    <router-view></router-view>
-  </div>
+    <div>
+        <TheHeadingNavbar></TheHeadingNavbar>
+        <router-view></router-view>
+        <Footer></Footer>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+Footer {
+    /* footer를 aside위에 올리기 위해 사용(부유객체) */
+    position: absolute;
+    height: 60px;
+    width: 100%;
+    padding: 0 25px;
+    line-height: 60px;
+    /* border-top: 1px solid #dee5e7; */
+}
+</style>
---
+++ enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/components/layout/Footer.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+    <footer class="navbar navbar-expand-sm navbar-light bg-light justify-content-center">
+        <div class="row">
+            <ul class="navbar-nav">
+                <li><a href="#" class="nav-link text-secondary">개인정보처리방침</a></li>
+                <li><a href="#" class="nav-link text-secondary">이용약관</a></li>
+                <li><label class="nav-link text-secondary">&copy; SSAFY Corp.</label></li>
+            </ul>
+        </div>
+    </footer>
+</template>
+
+<style scoped></style>
