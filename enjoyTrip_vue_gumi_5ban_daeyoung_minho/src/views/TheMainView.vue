1ea1ee7285cd174540ef0fcb82d3e1c7ed6dd3b7
김대영 <ssafy@ssafy>
2023-11-10 15:16:44 +0900

    Feat : 메인 페이지 구현

--- enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/views/TheMainView.vue
+++ enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/views/TheMainView.vue
@@ -1,7 +1,11 @@
 <script setup></script>
 
 <template>
-  <div>Main 화면 입니다.</div>
+    <div class="container text-center mt-3">
+        <div class="alert alert-info" role="alert">여행을 떠나 봐요</div>
+        <router-view></router-view>
+    </div>
+    <img src="@/assets/main.jpg" class="img-fluid mx-auto d-block" />
 </template>
 
 <style scoped></style>
