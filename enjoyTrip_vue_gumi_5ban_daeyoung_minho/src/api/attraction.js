cc1d0b0b8f51907ac4c6eae20bcddbbf5590ba43
김대영 <ssafy@ssafy>
2023-11-13 17:39:06 +0900

    Feat : 관광지 정보 조회 구현

--- enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/api/attraction.js
+++ enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/api/attraction.js
@@ -2,20 +2,16 @@ import { attractionAxios } from "@/util/http-commons";
 
 const attraction = attractionAxios();
 
-function areaCode1 (param, success, fail) {
+function areaCode1(param, success, fail) {
     attraction.get(`/areaCode1`, { params: param }).then(success).catch(fail);
-  }
+}
 
-  function areaBasedList1 (param, success, fail) {
+function areaBasedList1(param, success, fail) {
     attraction.get(`/areaBasedList1`, { params: param }).then(success).catch(fail);
-  }
+}
 
-  function searchKeyword1 (param, success, fail) {
+function searchKeyword1(param, success, fail) {
     attraction.get(`/searchKeyword1`, { params: param }).then(success).catch(fail);
-  }
+}
 
-  export {
-    sidoList,
-    areaBasedList1,
-    searchKeyword1,
-  };
+export { areaCode1, areaBasedList1, searchKeyword1 };
--- enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/components/attraction/AttractionList.vue
+++ enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/components/attraction/AttractionList.vue
@@ -8,17 +8,19 @@ const { VITE_ATTRACTION_API_SERVICE_KEY } = import.meta.env;
 
 const sidos = ref([]);
 
+const attractionList = ref([]);
+
 const searchParam = ref({
-    serviceKey: VITE_ATTRACTION_API_SERVICE_KEY,
     MobileOS: "ETC",
     MobileApp: "EnjoyTrip",
     _type: "json",
     listYN: "Y",
     arrange: "A",
+    contentTypeId: "0",
+    areaCode: "",
+    serviceKey: VITE_ATTRACTION_API_SERVICE_KEY,
     numOfRows: "",
     pageNo: "",
-    areaCode: "",
-    contentTypeId: "",
     keyword: "",
 });
 
@@ -28,7 +30,7 @@ onMounted(() => {
 
 const getSidoList = () => {
     console.log("시도 정보를 얻어오자!");
-    sidos.value.push({"rnum" : null, "code" : '', "name" : "검색 할 지역 선택"});
+    sidos.value.push({ rnum: null, code: "", name: "검색 할 지역 선택" });
     areaCode1(
         {
             serviceKey: VITE_ATTRACTION_API_SERVICE_KEY,
@@ -36,9 +38,9 @@ const getSidoList = () => {
             MobileApp: "EnjoyTrip",
             _type: "json",
             numOfRows: "20",
-        }
-    ,
+        },
         ({ data }) => {
+            console.log("areaCode1 api 호출");
             sidos.value.push(...data.response.body.items.item);
             console.log("시도 정보 획득", sidos.value);
         },
@@ -46,6 +48,44 @@ const getSidoList = () => {
             console.log(error);
         }
     );
+};
+
+const getAttractionList = () => {
+    console.log(searchParam.value);
+    if (searchParam.value.keyword === "")
+        areaBasedList1(
+            {
+                MobileOS: "ETC",
+                MobileApp: "EnjoyTrip",
+                _type: "json",
+                listYN: "Y",
+                arrange: "A",
+                contentTypeId: searchParam.value.contentTypeId,
+                areaCode: searchParam.value.areaCode,
+                serviceKey: VITE_ATTRACTION_API_SERVICE_KEY,
+                numOfRows: "",
+                pageNo: "",
+            },
+            ({ data }) => {
+                console.log("areaBasedList1 api 호출");
+                attractionList.value.push(...data.response.body.items.item);
+                console.log("관광지 정보 획득", attractionList.value);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    else
+        searchKeyword1(
+            searchParam.value,
+            ({ data }) => {
+                console.log("searchKeyword1 api 호출");
+                attractionList.value.push(...data.response.body.items.item);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
 };
 </script>
 
@@ -69,26 +109,51 @@ export default {
             <div class="col-lg-10">
                 <!-- 관광지 검색 start -->
                 <form class="d-flex my-3" onsubmit="return false;" role="search">
-                    <select id="search-area" class="form-select me-2">
-                        <option v-for="sido in sidos" :key="sido.code" :sido="sido"
-                        :disabled="sido.code === '' ? true : false"
-                        :selected="sido.code === '' ? true : false">
+                    <select
+                        id="search-area"
+                        class="form-select me-2"
+                        v-model="searchParam.areaCode"
+                    >
+                        <option
+                            v-for="sido in sidos"
+                            :key="sido.code"
+                            :sido="sido"
+                            :value="sido.code"
+                            :disabled="sido.code === '' ? true : false"
+                            :selected="sido.code === '' ? true : false"
+                        >
                             {{ sido.name }}
                         </option>
                     </select>
-                    <select id="search-content-id" class="form-select me-2">
-                        <option value="0" selected>관광지 유형</option>
-                        <option value="12">관광지</option>
-                        <option value="14">문화시설</option>
-                        <option value="15">축제공연행사</option>
-                        <option value="25">여행코스</option>
-                        <option value="28">레포츠</option>
-                        <option value="32">숙박</option>
-                        <option value="38">쇼핑</option>
-                        <option value="39">음식점</option>
+                    <select
+                        id="search-content-id"
+                        class="form-select me-2"
+                        v-model="searchParam.contentTypeId"
+                    >
+                        <option :value="0" :disabled="true">관광지 유형</option>
+                        <option :value="12">관광지</option>
+                        <option :value="14">문화시설</option>
+                        <option :value="15">축제공연행사</option>
+                        <option :value="25">여행코스</option>
+                        <option :value="28">레포츠</option>
+                        <option :value="32">숙박</option>
+                        <option :value="38">쇼핑</option>
+                        <option :value="39">음식점</option>
                     </select>
-                    <input id="search-keyword" class="form-control me-2" type="search" placeholder="검색어" aria-label="검색어" />
-                    <button id="btn-search" class="btn btn-outline-success" type="button">
+                    <input
+                        id="search-keyword"
+                        class="form-control me-2"
+                        type="search"
+                        placeholder="검색어"
+                        aria-label="검색어"
+                        v-model="searchParam.keyword"
+                    />
+                    <button
+                        id="btn-search"
+                        class="btn btn-outline-success"
+                        type="button"
+                        @click="getAttractionList"
+                    >
                         검색
                     </button>
                 </form>
@@ -97,8 +162,8 @@ export default {
                 <!-- kakao map end -->
                 <!-- 관광지 검색 start -->
                 <div class="row">
-                    <table class="table table-striped" style="display: none">
-                        <thead>
+                    <table class="table table-striped" v-show="attractionList.length != 0">
+                        <thead class="text-center">
                             <tr>
                                 <th>대표이미지</th>
                                 <th>관광지명</th>
@@ -107,7 +172,20 @@ export default {
                                 <th>경도</th>
                             </tr>
                         </thead>
-                        <tbody id="trip-list"></tbody>
+                        <tbody id="trip-list">
+                            <tr
+                                class="text-center"
+                                v-for="attraction in attractionList"
+                                :key="attraction.contentid"
+                                :attraction="attraction"
+                            >
+                                <td><img :src="attraction.firstimage" style="width: 100px" /></td>
+                                <td>{{ attraction.title }}</td>
+                                <td>{{ attraction.addr1 }}</td>
+                                <td>{{ attraction.mapx }}</td>
+                                <td>{{ attraction.mapy }}</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 <!-- 관광지 검색 end -->
