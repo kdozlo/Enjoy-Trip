9d0d7aed41833fb44aff62703eaaf15d4c52817b
김대영 <eodud5730@naver.com>
2023-11-14 15:38:08 +0900

    Feat : 카카오맵 구현

--- enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/App.vue
+++ enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/App.vue
@@ -5,21 +5,11 @@ import Footer from "@/components/layout/Footer.vue";
 </script>
 
 <template>
-    <div>
+    <div id="app">
         <TheHeadingNavbar></TheHeadingNavbar>
         <router-view></router-view>
         <Footer></Footer>
     </div>
 </template>
 
-<style scoped>
-Footer {
-    /* footer를 aside위에 올리기 위해 사용(부유객체) */
-    position: absolute;
-    height: 60px;
-    width: 100%;
-    padding: 0 25px;
-    line-height: 60px;
-    /* border-top: 1px solid #dee5e7; */
-}
-</style>
+<style scoped></style>
