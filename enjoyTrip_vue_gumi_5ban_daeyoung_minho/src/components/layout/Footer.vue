ddcb18ecf497661d97e25ed43ebb8b3876fc1c4c
김대영 <eodud5730@naver.com>
2023-11-15 13:30:03 +0900

    Design : footer 디자인 변경

--- enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/components/layout/Footer.vue
+++ enjoyTrip_vue_gumi_5ban_daeyoung_minho/src/components/layout/Footer.vue
@@ -3,11 +3,22 @@
 <template>
     <div style="height: 40px"></div>
     <footer class="navbar navbar-expand-sm bg-light justify-content-center">
-            <ul class="navbar-nav">
-                <li><a href="#" class="nav-link text-secondary">개인정보처리방침</a></li>
-                <li><a href="#" class="nav-link text-secondary">이용약관</a></li>
-                <li><label class="nav-link text-secondary">&copy; SSAFY Corp.</label></li>
-            </ul>
+        <ul class="navbar-nav">
+            <li><a href="#" class="nav-link text-secondary">개인정보처리방침</a></li>
+            <li><a href="#" class="nav-link text-secondary">이용약관</a></li>
+            <li>
+                <label class="nav-link text-secondary">
+                    &copy;
+                    <img
+                        src="@/assets/ssafy-hand-draw.png"
+                        class="rounded mx-auto"
+                        alt="..."
+                        width="80"
+                    />
+                    Corp.
+                </label>
+            </li>
+        </ul>
     </footer>
 </template>
 
