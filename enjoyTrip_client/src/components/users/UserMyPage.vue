3762d4e59ea07b67c256a4bef6780659f42180df
김대영 <eodud5730@naver.com>
2023-11-20 09:47:10 +0900

    Feat : 마이페이지 구현

--- enjoyTrip_client/src/components/users/UserMyPage.vue
+++ enjoyTrip_client/src/components/users/UserMyPage.vue
@@ -1,40 +1,49 @@
-<script setup></script>
+<script setup>
+import { storeToRefs } from "pinia";
+import { useMemberStore } from "@/stores/member";
+
+const memberStore = useMemberStore();
+const { userInfo } = storeToRefs(memberStore);
+console.log(userInfo.value);
+</script>
 
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-10">
-        <h2 class="my-3 py-3 shadow-sm bg-light text-center">
-          <mark class="orange">내정보</mark>
-        </h2>
-      </div>
-      <div class="col-lg-10">
-        <div class="card mt-3 m-auto" style="max-width: 700px">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img
-                src="https://source.unsplash.com/random/250x250/?food"
-                class="img-fluid rounded-start"
-                alt="..."
-              />
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <h2 class="my-3 py-3 shadow-sm bg-light text-center">
+                    <mark class="orange">내정보</mark>
+                </h2>
             </div>
-            <div class="col-md-8">
-              <div class="card-body text-start">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">SSAFY</li>
-                  <li class="list-group-item">김싸피</li>
-                  <li class="list-group-item">ssafy@ssafy.com</li>
-                </ul>
-              </div>
+            <div class="col-lg-10">
+                <div class="card mt-3 m-auto" style="max-width: 700px">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img
+                                src="https://source.unsplash.com/random/250x250/?food"
+                                class="img-fluid rounded-start"
+                                alt="..."
+                            />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body text-start">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">{{ userInfo.userId }}</li>
+                                    <li class="list-group-item">{{ userInfo.userName }}</li>
+                                    <li class="list-group-item">
+                                        {{ userInfo.emailId }}@{{ userInfo.emailDomain }}
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-outline-secondary mt-2">수정</button>
+                </div>
             </div>
-          </div>
         </div>
-        <div>
-          <button type="button" class="btn btn-outline-secondary mt-2">수정</button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <style scoped></style>
