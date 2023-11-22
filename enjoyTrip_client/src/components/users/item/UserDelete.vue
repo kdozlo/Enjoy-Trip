7eb7f64fa05a773b7674a23089be86d54742ccab
kdozlo <eodud5730@naver.com>
2023-11-22 15:30:57 +0900

    Design : 회원 탈퇴 모달 디자인 수정

--- enjoyTrip_client/src/components/users/item/UserDelete.vue
+++ enjoyTrip_client/src/components/users/item/UserDelete.vue
@@ -110,8 +110,19 @@ const removeUser = async () => {
 .black-bg {
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.519);
     position: fixed;
+    left: 0;
+    top: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(1.5px);
+    -webkit-backdrop-filter: blur(1.5px);
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
     z-index: 1000;
 }
 
